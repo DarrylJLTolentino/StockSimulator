d31742d240be4ad2b6a7e5938ef164e9a9fae95a
Darryl Tolentino <darryl.tolentino@gmail.com>
2020-03-06 19:05:43 -0800

    Adding HttpClient to app.components.ts, HttpClientModule to app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'StockSimulator';
+
+  title = "Stock Simulator";
+
+  constructor(private http: HttpClient) {
+
+    var link = 'https://staging-api.brainbase.com/stocks.php';
+
+    this.http.get(link).toPromise().then(data => {
+      console.log(data);
+    });
+  }
+
 }
