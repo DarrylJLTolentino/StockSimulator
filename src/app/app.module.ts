9d65191b24f32c3eb6c5a559eebd72e1bda6c321
Darryl Tolentino <darryl.tolentino@gmail.com>
2020-03-06 23:28:19 -0800

    Adding Date component to src directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DateComponent } from './date/date.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
