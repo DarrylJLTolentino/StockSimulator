380a5ce989db8b0a3fb584cb94cd350ad19f805f
Darryl Tolentino <darryl.tolentino@gmail.com>
2020-03-10 21:00:16 -0700

    Adding MDBootstrap to project and changing Table to MDBootstrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Necessary to import CommonModule in order to utilize *ngFor within app.component.html
 import { CommonModule } from '@angular/common';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // Necessary to import HttpClientModule in order to use a REST API call within app.component.ts
 import { HttpClientModule } from '@angular/common/http';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 @NgModule({
@@ -18,7 +18,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
