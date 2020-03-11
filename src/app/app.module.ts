e08529e26bb6d6afb26aa9bf7640fdc57e8ca753
Darryl Tolentino <darryl.tolentino@gmail.com>
2020-03-10 17:05:17 -0700

    Adding comments to app.module.ts for clarity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Necessary to import CommonModule in order to utilize *ngFor within app.component.html
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Necessary to import HttpClientModule in order to use a REST API call within app.component.ts
 import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // Need to import HttpClientModule and CommonModule here as well to make the exported declarations of other modules available in the current module
   imports: [
     BrowserModule,
     AppRoutingModule,
